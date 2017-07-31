86d27a48a4c69586d65402c6ca5dcdc6c8130aa0
Daniil159x <daniil.ru-m@mail.ru>
2017-07-31 11:44:48 +0700

    load files

---
+++ unicodesetting.hpp
@@ -0,0 +1,66 @@
+﻿#ifndef UNICODESETTING_HPP
+#define UNICODESETTING_HPP
+
+#ifdef WIN32
+    #include "winunicodebuf.hpp"
+    #include <Windows.h>
+#endif
+
+#include <iostream>
+#include <codecvt>
+#include <memory>
+
+template <typename CHAR_T = wchar_t>
+std::locale configureLocale_Unicode(std::locale base = std::locale::classic(),
+                            std::initializer_list<std::locale::category> category_copy = {std::locale::ctype}){
+
+    if(category_copy.size() > 0) {
+
+        constexpr const char* names_locs[] = {
+            // POSIX OS
+            "ru_RU.UTF-8",
+            "en_US.UTF-8",
+            // windows
+            "Russia_Russia"
+        };
+
+        std::locale loc_new; // one of the 'names_locs' or the global
+
+        for(size_t i = 0; i < sizeof(names_locs)/sizeof(const char*); ++i){
+            try {
+                loc_new = std::locale( names_locs[i] );
+                break;
+            } catch (...) {}
+        }
+
+        for(auto category : category_copy){
+            base = std::locale(base, loc_new, category);
+        }
+    }
+
+    return { base, new std::codecvt_utf8<CHAR_T> };
+}
+
+
+std::locale InitUnicodeStreams(std::locale base = {}){
+#ifdef WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+	DWORD consoleMode;
+	if (!!GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &consoleMode)) {
+		std::wcin.rdbuf(new details::winUnicodeBuf(*std::wcin.rdbuf()));
+	}
+#endif
+
+    auto locUnicode = configureLocale_Unicode(base, {});
+
+    std::wcin.imbue(locUnicode);
+    std::wcout.imbue(locUnicode);
+    std::wcerr.imbue(locUnicode);
+    std::wclog.imbue(locUnicode);
+
+    return locUnicode;
+}
+
+#endif // UNICODESETTING_HPP
---
+++ winunicodebuf.hpp
@@ -0,0 +1,102 @@
+﻿#ifndef WINUNICODEBUF_HPP
+#define WINUNICODEBUF_HPP
+#ifdef WIN32
+
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+
+#include <streambuf>
+
+namespace details {
+    class winUnicodeBuf : public std::wstreambuf
+    {
+        using Base = std::wstreambuf;
+        using Traits = Base::traits_type;
+
+    public:
+        winUnicodeBuf(const Base& buf) : Base(std::move(buf)), m_buffer() {}
+
+    protected:
+        virtual std::streamsize xsgetn(wchar_t* pBuffer, std::streamsize n) override
+        {
+            const wchar_t ctrlZ = wchar_t(1 + ('Z' - 'A'));
+
+            DWORD nCharactersRead = 0;
+
+            const bool readSucceeded = !!ReadConsoleW(
+                GetStdHandle(STD_INPUT_HANDLE), pBuffer,
+                static_cast<DWORD>(n), &nCharactersRead, nullptr
+            );
+
+            if (readSucceeded)
+            {
+                wchar_t const* const pCleanEnd =
+                    myRemove(pBuffer, pBuffer + nCharactersRead);
+
+                nCharactersRead = pCleanEnd - pBuffer;
+
+                bool const isInteractiveEOF =
+                    (nCharactersRead == 2 && pBuffer[0] == ctrlZ && pBuffer[1] == '\n');
+
+                return (isInteractiveEOF ? 0 : static_cast<std::streamsize>(nCharactersRead));
+            }
+            return 0;
+        }
+
+        virtual int_type underflow() override
+        {
+            if (pCurrent() == 0 || pCurrent() >= pEnd())
+            {
+                const std::streamsize nCharactersRead =
+                    xsgetn(m_buffer, m_bufferSize);
+
+                if (nCharactersRead > 0)
+                {
+                    setg(m_buffer, m_buffer,
+                            m_buffer + m_bufferSize);
+                }
+            }
+
+            if (pCurrent() == 0 || pCurrent() >= pEnd())
+            {
+                return Traits::eof();
+            }
+            return Traits::to_int_type(*pCurrent());
+        }
+
+    private:
+        static const std::streamsize m_bufferSize = 128;
+        wchar_t m_buffer[m_bufferSize];
+
+        // std::remove
+        template <typename It>
+        static inline It myRemove(It first, It last) noexcept
+        {
+            const auto value = L'\r';
+
+            for (; first != last; ++first) {
+                if (*first == value) {
+                    break;
+                }
+            }
+
+            if (first != last) {
+                for (auto i = first; ++i != last; ) {
+                    if (!(*i == value)) {
+                        *first++ = std::move(*i);
+                    }
+                }
+            }
+            return first;
+        }
+
+        inline wchar_t* pStart()   const noexcept { return eback(); }
+        inline wchar_t* pCurrent() const noexcept { return gptr();  }
+        inline wchar_t* pEnd()     const noexcept { return egptr(); }
+    };
+} // namespace details
+
+
+#endif
+#endif // WINUNICODEBUF_HPP
